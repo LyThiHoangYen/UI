f12a4fdd2677511b7a9fa750e1586f1ff6d104a4
LyThiHoangYen <lythihoangyen20052003@gmail.com>
2024-12-07 13:43:31 +0700

    push

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000/api/v1';
+export const BASE_URL = 'https://method2aapi-e6bqfmgkbdcwcyc9.eastasia-01.azurewebsites.net/api/v1';
 export const token = localStorage.getItem('token');
